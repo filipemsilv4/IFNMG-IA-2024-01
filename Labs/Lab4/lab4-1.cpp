b0a2e00f391a27ce07ce4c2e6b043591f695a648
Paulo Filipe Moreira da Silva <59426206+filipemsilv4@users.noreply.github.com>
2024-03-14 10:25:50 -0300

    Small cleanup

--- Labs/Lab4/lab4-1.cpp
+++ Labs/Lab4/lab4-1.cpp
@@ -17,33 +17,33 @@ enum class Player { None, X, O };
 
 class TicTacToe {
     protected:
-        vector<vector<Player>> tabuleiro;
+        vector<vector<Player>> board;
     public:
-        // #Inicializa o tabuleiro
+        // #Inicializa o board
         TicTacToe() {
-            tabuleiro = {
+            board = {
                 {Player::None, Player::None, Player::None},
                 {Player::None, Player::None, Player::None},
                 {Player::None, Player::None, Player::None}
             };
         }
 
-        // Retorna o tabuleiro
-        vector<vector<Player>> getTabuleiro() {
-            return tabuleiro;
+        // Retorna o board
+        vector<vector<Player>> getboard() {
+            return board;
         }
 
         // #Retorna X ou O, dependendo de quem é o jogador atual
-        // def jogador(tabuleiro):
-        Player jogador(vector<vector<Player>> tabuleiro) {
+        // def jogador(board):
+        Player jogador(vector<vector<Player>> board) {
             int x = 0;
             int o = 0;
 
-            for (int i = 0; i < tabuleiro.size(); ++i) {
-                for (int j = 0; j < tabuleiro[i].size(); ++j) {
-                    if (tabuleiro[i][j] == Player::X) {
+            for (int i = 0; i < board.size(); ++i) {
+                for (int j = 0; j < board[i].size(); ++j) {
+                    if (board[i][j] == Player::X) {
                         ++x;
-                    } else if (tabuleiro[i][j] == Player::O) {
+                    } else if (board[i][j] == Player::O) {
                         ++o;
                     }
                 }
@@ -53,13 +53,13 @@ class TicTacToe {
         }
 
         // #Retorna todas as jogadas disponíveis
-        // def acoes(tabuleiro):
-        vector<pair<int, int>> acoes(vector<vector<Player>> tabuleiro) {
+        // def acoes(board):
+        vector<pair<int, int>> acoes(vector<vector<Player>> board) {
             vector<pair<int, int>> availableActions;
 
-            for (int i = 0; i < tabuleiro.size(); ++i) {
-                for (int j = 0; j < tabuleiro[i].size(); ++j) {
-                    if (tabuleiro[i][j] == Player::None) {
+            for (int i = 0; i < board.size(); ++i) {
+                for (int j = 0; j < board[i].size(); ++j) {
+                    if (board[i][j] == Player::None) {
                         availableActions.push_back({i, j});
                     }
                 }
@@ -68,59 +68,59 @@ class TicTacToe {
             return availableActions;
         }
 
-        // #Retorna o tabuleiro que resulta ao fazer uma jogada do vetor de ações
-        // def resultado(tabuleiro, acao):
-        vector<vector<Player>> resultado(vector<vector<Player>> tabuleiro, pair<int, int> acao) {
-            vector<vector<Player>> novoTabuleiro = tabuleiro;
+        // #Retorna o board que resulta ao fazer uma jogada do vetor de ações
+        // def resultado(board, acao):
+        vector<vector<Player>> resultado(vector<vector<Player>> board, pair<int, int> acao) {
+            vector<vector<Player>> novoboard = board;
             // Check if the action is valid
-            if (novoTabuleiro[acao.first][acao.second] != Player::None) {
+            if (novoboard[acao.first][acao.second] != Player::None) {
                 cout << "Jogada inválida!" << endl;
                 throw "Jogada inválida!";
             }
 
-            novoTabuleiro[acao.first][acao.second] = jogador(tabuleiro);
+            novoboard[acao.first][acao.second] = jogador(board);
             
-            return novoTabuleiro;
+            return novoboard;
         }
 
         // #Retorna o ganhador, se houver
-        // def ganhador(tabuleiro):
-        Player ganhador(vector<vector<Player>> tabuleiro) {
+        // def ganhador(board):
+        Player ganhador(vector<vector<Player>> board) {
             // Check the rows
             for (int i = 0; i < 3; ++i) {
-                if (tabuleiro[i][0] != Player::None && tabuleiro[i][0] == tabuleiro[i][1] && tabuleiro[i][0] == tabuleiro[i][2]) {
-                    return tabuleiro[i][0];
+                if (board[i][0] != Player::None && board[i][0] == board[i][1] && board[i][0] == board[i][2]) {
+                    return board[i][0];
                 }
             }
 
             // Check the columns
             for (int i = 0; i < 3; ++i) {
-                if (tabuleiro[0][i] != Player::None && tabuleiro[0][i] == tabuleiro[1][i] && tabuleiro[0][i] == tabuleiro[2][i]) {
-                    return tabuleiro[0][i];
+                if (board[0][i] != Player::None && board[0][i] == board[1][i] && board[0][i] == board[2][i]) {
+                    return board[0][i];
                 }
             }
 
             // Check one diagonal
-            if (tabuleiro[0][0] != Player::None && tabuleiro[0][0] == tabuleiro[1][1] && tabuleiro[0][0] == tabuleiro[2][2]) {
-                return tabuleiro[0][0];
+            if (board[0][0] != Player::None && board[0][0] == board[1][1] && board[0][0] == board[2][2]) {
+                return board[0][0];
             }
 
             // Check other diagonal
-            if (tabuleiro[0][2] != Player::None && tabuleiro[0][2] == tabuleiro[1][1] && tabuleiro[0][2] == tabuleiro[2][0]) {
-                return tabuleiro[0][2];
+            if (board[0][2] != Player::None && board[0][2] == board[1][1] && board[0][2] == board[2][0]) {
+                return board[0][2];
             }
 
             return Player::None;
         }
 
         // #Retorna Verdadeiro se o jogo acabou, Falso caso contrário
-        // def final(tabuleiro):
-        bool final(vector<vector<Player>> tabuleiro) {
-            for (int i = 0; i < tabuleiro.size(); ++i) {
-                for (int j = 0; j < tabuleiro[i].size(); ++j) {
-                    if (tabuleiro[i][j] == Player::None) {
+        // def final(board):
+        bool final(vector<vector<Player>> board) {
+            for (int i = 0; i < board.size(); ++i) {
+                for (int j = 0; j < board[i].size(); ++j) {
+                    if (board[i][j] == Player::None) {
                         // The game may be over if someone has won
-                        if (ganhador(tabuleiro) != Player::None) {
+                        if (ganhador(board) != Player::None) {
                             return true;
                         }
                         return false;
@@ -131,10 +131,10 @@ class TicTacToe {
         }
 
         // #Retorna 1 se X ganhou, -1 se 0 ganhou, 0 caso contrário.
-        // def custo(tabuleiro):
+        // def custo(board):
 
-        int custo(vector<vector<Player>> tabuleiro, int depth = 0) {
-            Player winner = ganhador(tabuleiro);
+        int custo(vector<vector<Player>> board, int depth = 0) {
+            Player winner = ganhador(board);
             if (winner == Player::X) {
                 return 1;
             } else if (winner == Player::O) {
@@ -145,21 +145,21 @@ class TicTacToe {
         }
 
         // #Retorna a jogada ótima para o jogador atual
-        // def minimax(tabuleiro):
+        // def minimax(board):
         // pair meaning: (score, (row, col))
-        pair<int, pair<int, int>> minimax(vector<vector<Player>> tabuleiro, int depth, int alpha, int beta, bool maximizingPlayer) {
-            if (final(tabuleiro) || depth == 0) {
-                return {custo(tabuleiro, depth), {-1, -1}};
+        pair<int, pair<int, int>> minimax(vector<vector<Player>> board, int depth, int alpha, int beta, bool maximizingPlayer) {
+            if (final(board) || depth == 0) {
+                return {custo(board, depth), {-1, -1}};
             }
 
-            vector<pair<int, int>> actions = acoes(tabuleiro);
+            vector<pair<int, int>> actions = acoes(board);
             pair<int, int> bestAction = {-1, -1};
 
             // Maximizing player
             if (maximizingPlayer) {
                 int maxEval = -INT_MAX;
                 for (int i = 0; i < actions.size(); ++i) {
-                    vector<vector<Player>> newBoard = resultado(tabuleiro, actions[i]);
+                    vector<vector<Player>> newBoard = resultado(board, actions[i]);
                     int eval = minimax(newBoard, depth - 1, alpha, beta, false).first;
                     if (eval > maxEval) {
                         maxEval = eval;
@@ -174,7 +174,7 @@ class TicTacToe {
             } else {
                 int minEval = INT_MAX;
                 for (int i = 0; i < actions.size(); ++i) {
-                    vector<vector<Player>> newBoard = resultado(tabuleiro, actions[i]);
+                    vector<vector<Player>> newBoard = resultado(board, actions[i]);
                     int eval = minimax(newBoard, depth - 1, alpha, beta, true).first;
                     if (eval < minEval) {
                         minEval = eval;
@@ -190,17 +190,17 @@ class TicTacToe {
         }
 
         // #Executar jogada da IA
-        void jogadaIA() {
+        void AITurn() {
             //Player::X starts the game and is the maximizing player
-            bool maximizingPlayer = jogador(tabuleiro) == Player::X;
-            pair<int, pair<int, int>> bestMove = minimax(tabuleiro, INT_MAX, -INT_MAX, INT_MAX, maximizingPlayer);
-            tabuleiro = resultado(tabuleiro, bestMove.second);
+            bool maximizingPlayer = jogador(board) == Player::X;
+            pair<int, pair<int, int>> bestMove = minimax(board, INT_MAX, -INT_MAX, INT_MAX, maximizingPlayer);
+            board = resultado(board, bestMove.second);
         }
 
         // #Executar jogada do jogador
-        void jogadaJogador(int row, int col) {
+        void playerTurn(int row, int col) {
             try {
-                tabuleiro = resultado(tabuleiro, {row, col});
+                board = resultado(board, {row, col});
             } catch (const char* msg) {
                 throw msg;
             }
@@ -209,13 +209,13 @@ class TicTacToe {
         // Print the board
         void printBoard() {
             cout << "\033[1;32m   1   2   3\033[0m" << endl;
-            for (int i = 0; i < tabuleiro.size(); ++i) {
+            for (int i = 0; i < board.size(); ++i) {
                 // Print the indexes
                 cout << "\033[1;32m" << i + 1 << "  \033[0m";
-                for (int j = 0; j < tabuleiro[i].size(); ++j) {
-                    if (tabuleiro[i][j] == Player::None) {
+                for (int j = 0; j < board[i].size(); ++j) {
+                    if (board[i][j] == Player::None) {
                         cout << "\033[1;37m \033[0m";
-                    } else if (tabuleiro[i][j] == Player::X) {
+                    } else if (board[i][j] == Player::X) {
                         cout << "\033[1;34mX\033[0m";
                     } else {
                         cout << "\033[1;31mO\033[0m";
@@ -230,8 +230,8 @@ class TicTacToe {
                 }
             }
         }
-        // def maxValor(tabuleiro):
-        // def minValor(tabuleiro):
+        // def maxValor(board):
+        // def minValor(board):
 };
 
 // Implements the changes needed for the undo feature
@@ -240,47 +240,42 @@ class StrangeTicTacToe : public TicTacToe {
         vector<vector<vector<Player>>> history;
     public:
         StrangeTicTacToe() : TicTacToe() {
-            push_to_history(getTabuleiro());
+            push_to_history(getboard());
         }
 
         void push_to_history(vector<vector<Player>> state){
             history.push_back(state);
         }
 
-        // void jogadaIA() {
-        //     bool maximizingPlayer = jogador(tabuleiro) == Player::X;
-        //     pair<int, pair<int, int>> bestMove = minimax(tabuleiro, INT_MAX, -INT_MAX, INT_MAX, maximizingPlayer);
-        // }
-
-        void jogadaIA(){
-            // Undo the last player move, find the worst move for the player and make it
+        void AITurn(){
+            // Check if there is a previous state
             if(history.size() > 1){
-                tabuleiro = history[history.size() - 2]; // Get the previous state
+                board = history[history.size() - 2]; // Get the previous state
                 history.pop_back(); // Remove the current state
 
                 // We need to find the worst move for the player, so if the player is the maximizing player, we need to find the move that minimizes the player's score
-                bool maximizingPlayer = jogador(tabuleiro) == Player::X; //Player::X starts the game and is the maximizing player
-                pair<int, pair<int, int>> bestMove = minimax(tabuleiro, INT_MAX, -INT_MAX, INT_MAX, !maximizingPlayer);
-                tabuleiro = resultado(tabuleiro, bestMove.second);
-                push_to_history(tabuleiro);
+                bool maximizingPlayer = jogador(board) == Player::X; //Player::X starts the game and is the maximizing player
+                pair<int, pair<int, int>> bestMove = minimax(board, INT_MAX, -INT_MAX, INT_MAX, !maximizingPlayer);
+                board = resultado(board, bestMove.second);
+                push_to_history(board);
 
                 // Now we need to find the best move for the AI and make it
-                bestMove = minimax(tabuleiro, INT_MAX, -INT_MAX, INT_MAX, !maximizingPlayer);
-                tabuleiro = resultado(tabuleiro, bestMove.second);
-                push_to_history(tabuleiro);
+                bestMove = minimax(board, INT_MAX, -INT_MAX, INT_MAX, !maximizingPlayer);
+                board = resultado(board, bestMove.second);
+                push_to_history(board);
 
             } else { // If there is no previous state, just play normally
-                bool maximizingPlayer = jogador(tabuleiro) == Player::X; //Player::X starts the game and is the maximizing player
-                pair<int, pair<int, int>> bestMove = minimax(tabuleiro, INT_MAX, -INT_MAX, INT_MAX, maximizingPlayer);
-                tabuleiro = resultado(tabuleiro, bestMove.second);
-                push_to_history(tabuleiro);
+                bool maximizingPlayer = jogador(board) == Player::X; //Player::X starts the game and is the maximizing player
+                pair<int, pair<int, int>> bestMove = minimax(board, INT_MAX, -INT_MAX, INT_MAX, maximizingPlayer);
+                board = resultado(board, bestMove.second);
+                push_to_history(board);
             }
         }
 
-        void jogadaJogador(int row, int col) {
+        void playerTurn(int row, int col) {
             try {
-                tabuleiro = resultado(tabuleiro, {row, col});
-                push_to_history(tabuleiro);
+                board = resultado(board, {row, col});
+                push_to_history(board);
             } catch (const char* msg) {
                 throw msg;
             }
@@ -292,26 +287,26 @@ int main() {
     // clear the screen
     cout << "\033[2J\033[1;1H";
 
-    while (!game.final(game.getTabuleiro())) {
-        cout << "Tabuleiro atual: " << endl;
-        vector<vector<Player>> tabuleiro = game.getTabuleiro();
+    while (!game.final(game.getboard())) {
+        cout << "board atual: " << endl;
+        vector<vector<Player>> board = game.getboard();
         game.printBoard();
 
-        if (game.jogador(tabuleiro) == Player::X) {
+        if (game.jogador(board) == Player::X) {
             int row, col;
             cout << "Vez do jogador (\033[1;34mX\033[0m)." << endl << "Escolha a linha (1-3): ";
             cin >> row;
             cout << "Escolha a coluna (1-3): ";
             cin >> col;
             try {
-                game.jogadaJogador(row - 1, col - 1);
+                game.playerTurn(row - 1, col - 1);
             } catch (const char* msg) {
                 cout << msg << endl;
                 continue;
             }
         } else {
             cout << "Vez da IA (\033[1;31mO\033[0m)." << endl;
-            game.jogadaIA();
+            game.AITurn();
         }
         cout << "\033[1;36m-----------------\033[0m" << endl;
         // clear the screen
@@ -321,9 +316,9 @@ int main() {
     cout << "Fim de jogo!" << endl;
 
     // Who won?
-    if (game.custo(game.getTabuleiro()) == 1) {
+    if (game.custo(game.getboard()) == 1) {
         cout << "Jogador (\033[1;34mX\033[0m) venceu!" << endl;
-    } else if (game.custo(game.getTabuleiro()) == -1) {
+    } else if (game.custo(game.getboard()) == -1) {
         cout << "IA (\033[1;31mO\033[0m) venceu!" << endl;
     } else {
         cout << "Empate!" << endl;
